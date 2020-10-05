50f8d328284a747222efd69dc724793b923318a3
Tom Wright <tom.suffern.wright@gmail.com>
2020-10-05 18:59:48 +1100

    Rename SAM template generator

--- sceptre/templates/SAM/BuildSAM.py
+++ sceptre/templates/SAM/BuildSAM.py
@@ -21,9 +21,15 @@ def sceptre_handler(sceptre_user_data):
 
   logger.info(f"Building '{project}' with command '{command}'")
 
-  package_process = subprocess.run(command.split(" "))
-  if package_process.returncode != 0:
-    raise Exception(f'Command failed with error code {package_process.returncode}')
+  try:
+    process = subprocess.run(command, capture_output=True, shell=True, text=True)
+    if process.returncode != 0:
+      raise Exception(f'Command failed with error code {process.returncode}')
 
-  with open(f"sceptre/templates/SAM/{project}/template.cfn.yaml", "r") as f:
-    return f.read()
+    with open(f"sceptre/templates/SAM/{project}/template.cfn.yaml", "r") as f:
+      return f.read()
+  except Exception as e:
+    logger.error("Error occurred during AWS SAM build", exc_info=e)
+    if process:
+      logger.error(f"Process standard output stream:\n{process.stdout}")
+      logger.error(f"Process standard error stream:\n{process.stderr}")
