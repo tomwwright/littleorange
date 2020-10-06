576355f40971b2b35052f35926373554cf014394
Tom Wright <tom.suffern.wright@gmail.com>
2020-10-07 10:57:37 +1100

    Modify GuardDuty Custom Resource to enable in admin account and store DetectorId in SSM

--- sceptre/templates/SAM/GuardDutyOrganization/src/app.py
+++ sceptre/templates/SAM/GuardDutyOrganization/src/app.py
@@ -10,6 +10,21 @@ logger.setLevel(logging.INFO)
 helper = CfnResource(json_logging=True, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120)
 
 
+def assume_role(accountId):
+  sts = boto3.client("sts")
+  credentials = sts.assume_role(RoleArn=f"arn:aws:iam::{accountId}:role/OrganizationAccountAccessRole", RoleSessionName="GuardDutyOrganization")
+
+  logger.info(f"Assumed OrganizationAccountAccessRole in {accountId} for access key: {credentials['Credentials']['AccessKeyId']}")
+
+  assumed = boto3.Session(
+      aws_access_key_id=credentials["Credentials"]["AccessKeyId"],
+      aws_secret_access_key=credentials["Credentials"]["SecretAccessKey"],
+      aws_session_token=credentials["Credentials"]["SessionToken"]
+  )
+
+  return assumed
+
+
 def parse_boolean_values(properties):
   if "AutoEnable" in properties:
     properties["AutoEnable"] = bool(util.strtobool(properties["AutoEnable"]))
@@ -33,25 +48,28 @@ def create(event, context):
 
   logger.info(f"GuardDuty EnableOrganizationAdminAccount Response: {response}")
 
-  sts = boto3.client("sts")
-  credentials = sts.assume_role(RoleArn=f"arn:aws:iam::{accountId}:role/OrganizationAccountAccessRole", RoleSession="GuardDutyOrganization")
+  assumed = assume_role(accountId)
 
-  logger.info(f"Assumed OrganizationAccountAccessRole in {accountId} for access key: {credentials['Credentials']['AccessKeyId']}")
-
-  assumed = boto3.Session(
-      aws_access_key_id=credentials["Credentials"]["AccessKeyId"],
-      aws_secret_access_key=credentials["Credentials"]["SecretAccessKey"],
-      aws_session_token=credentials["Credentials"]["SessionToken"]
-  )
   assumedGuardDuty = assumed.client("guardduty")
   response = assumedGuardDuty.list_detectors()
 
   logger.debug(f"GuardDuty [ASSUMED] ListDetectors Response: {response}")
 
-  detectorId = response["DetectorIds"][0]
+  configuration["DetectorId"] = response["DetectorIds"][0]
   response = assumedGuardDuty.update_organization_configuration(**configuration)
 
   logger.info(f"GuardDuty [ASSUMED] UpdateOrganizationConfiguration Response: {response}")
+
+  assumedSsm = assumed.client("ssm")
+  response = assumedSsm.put_parameter(
+      Name="/LittleOrange/GuardDuty/DetectorId",
+      Description="GuardDuty DetectorId configured when account enabled as Organization admin account",
+      Value=detectorId,
+      Type="String",
+      Overwrite=True
+  )
+
+  logger.info(f"SSM [ASSUMED] PutParameter Response: {response}")
 
 
 @helper.delete
@@ -63,6 +81,15 @@ def delete(event, context):
   response = guardduty.disable_organization_admin_account(AdminAccountId=accountId)
 
   logger.info(f"GuardDuty DisableOrganizationAdminAccount Response: {response}")
+
+  assumed = assume_role(accountId)
+
+  assumedSsm = assumed.client("ssm")
+  response = assumedSsm.delete_parameter(
+      Name="/LittleOrange/GuardDuty/DetectorId"
+  )
+
+  logger.info(f"SSM [ASSUMED] DeleteParameter Response: {response}")
 
 
 def handler(event, context):
